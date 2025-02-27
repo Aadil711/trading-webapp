8a6c152ec653bc726620c628fbbae64f5c3d8c9e
Aditi Suryawanshi <113235839+aditiids@users.noreply.github.com>
2025-02-27 22:31:21 +0530

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,1 +1,13 @@
-
+const Button = ({ children, onClick }) => {
+    return (
+      <button
+        onClick={onClick}
+        style = {{backgroundColor: "#125398"}}
+        className="w-full p-3 rounded-2xl text-white text-xl font-bold tracking-wide hover:bg-blue-700 transition duration-300"
+      >
+        {children}
+      </button>
+    );
+  };
+  
+  export default Button;
