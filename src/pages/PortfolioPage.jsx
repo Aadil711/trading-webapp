bacd9c0f123471f881bcc24b825fa6a67b6a7fd4
Aditi Suryawanshi <113235839+aditiids@users.noreply.github.com>
2025-02-28 07:48:10 +0530

    Create PortfolioPage.jsx

---
+++ src/pages/PortfolioPage.jsx
@@ -0,0 +1,9 @@
+const PortfolioPage = () => {
+    return (
+      <div className="min-h-screen flex justify-center items-center bg-[#125398] text-white">
+        <h1>Welcome to your Trading Portfolio!</h1>
+      </div>
+    );
+  };
+  
+  export default PortfolioPage;
